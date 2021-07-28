f93b6887ff8a404efd753048ba64f067df2d2c4b
LUKA <lukalavalferk@gmail.com>
2021-07-28 14:10:56 +0200

    update readme + change var names in display.py

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 When I report on my IT projects I like the person to have a global view of the project architecture. With this in mind, the DIRdisplayer describes the architecture of a file.
 
+## Run
 It is easily launched in the following way:
 `python3 display.py <path_to_dir> [path_to_file]`
 
 If path_to_file is not given as argument, the output is displayed on `stdout`, otherwise in the file given as argument.
+
+## More
+The DIRdisplayer doesn't display the hidden files (those who start with a '.'). But you can change that easily in the code. 
--- display.py
+++ display.py
@@ -6,20 +6,20 @@ path_to_file = 0
 tab = "| "
 
 # Display function display the organisation of the folder <path_to_folder> in the file <path_to_file> with <tab> as symbols
-def display(path_to_folder, path_to_file):
+def display(path_to_dir, path_to_file):
     if(path_to_folder[-1] != "/"):
         path_to_folder += "/"
 
     f = open(path_to_file, "a")
 
     def aux(path_to_folder, signs):
-        dir = os.listdir(path_to_folder)
+        dir = os.listdir(path_to_dir)
         dir.sort()
         for file in dir:
             if(file[0] != "."):
                 f.write(signs + file + "\n")
-                if(os.path.isdir(path_to_folder + file)):
-                    aux(path_to_folder + file + "/", signs + tab)
+                if(os.path.isdir(path_to_dir + file)):
+                    aux(path_to_dir + file + "/", signs + tab)
 
     aux(path_to_folder, "")
     f.close()
@@ -35,4 +35,4 @@ else:
         path_to_file = sys.argv[2]
 
 # Execute display
-display(path_to_folder, path_to_file)
+display(path_to_dir, path_to_file)
